5b5e928770e323c4d56bbfe1314c390d019f253f
JadeJade1122 <jadonalina@student.olympic.edu>
2026-04-24 12:57:26 -0700

    Tried to get an enemy sprite

---
+++ Final Project/Enemies.cpp
@@ -0,0 +1,16 @@
+#include "Enemies.hpp"
+#include <iostream>
+
+//Enemy::Enemy(const std::string& imagePath) : m_sprite(m_texture)
+//{
+//	m_sprite.setTexture(m_texture);
+//	m_sprite.setScale(sf::Vector2f(0.5f, .5f));
+//}
+//
+//void Enemy::draw(sf::RenderWindow& window)
+//{
+//	window.draw(m_sprite);
+//}
+
+
+//Not working, need to fix
--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -1,14 +1,22 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Enemies.hpp"
 
 int main()
 {
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
+    sf::RenderWindow window(sf::VideoMode({ 600, 800 }), "SFML window");
 
+    sf::Texture texture;
 
-    sf::Font font("C:/Windows/Fonts/arial.ttf");
-    sf::Text text(font, "Hello SFML", 50);
+    //Template for enemy sprite & texture 
+    texture.loadFromFile("./Images/Enemy.jpg");
+    sf::Sprite sprite(texture);
+    sprite.setScale(sf::Vector2f(0.1f, 0.15f));
+
+    //sf::Font font("C:/Windows/Fonts/arial.ttf");
+    //sf::Text text(font, "Hello SFML", 50);
 
     // Start the game loop
     while (window.isOpen())
@@ -24,9 +32,10 @@ int main()
         // Clear screen
         window.clear();
 
-        // Draw the string
-        window.draw(text);
+        window.draw(sprite);
 
+        //window.draw(text);
+        // 
         // Update the window
         window.display();
     }
