c93b927e7647d290700c6a275473275a47b07e85
Carson Bluhm <carsonbluhm@gmail.com>
2026-04-29 22:51:53 -0700

    updated asset manager
    Refactored texture loading to use move semantics for efficiency and added error handling for loading failures.

--- Final Project/AssetManager.cpp
+++ Final Project/AssetManager.cpp
@@ -4,11 +4,39 @@ std::vector<sf::Texture> Assets::tiles;
 
 void Assets::loadTextures() 
 {
-	tiles.push_back(sf::Texture(sf::Image({16, 16}, sf::Color::Transparent)));
-	tiles.push_back(sf::Texture("Assets/Tiles/dirt.png"));
-	tiles.push_back(sf::Texture("Assets/Tiles/grass.png"));
+  tiles.clear();
 
-	std::cout << "Textures Loaded" << std::endl;
+    tiles.emplace_back();
+
+
+	{
+		sf::Texture t;
+		if (!t.loadFromFile("Assets/Tiles/dirt.png")) std::cerr << "Failed to load Assets/Tiles/dirt.png\n";
+		tiles.push_back(std::move(t));
+	}
+
+
+	{
+		sf::Texture t;
+		if (!t.loadFromFile("Assets/Tiles/grass.png")) std::cerr << "Failed to load Assets/Tiles/grass.png\n";
+		tiles.push_back(std::move(t));
+	}
+
+
+	{
+		sf::Texture t;
+		if (!t.loadFromFile("Assets/DayNight/Andy Sun.png")) std::cerr << "Failed to load Assets/DayNight/Andy Sun.png\n";
+		tiles.push_back(std::move(t));
+	}
+
+
+	{
+		sf::Texture t;
+		if (!t.loadFromFile("Assets/DayNight/Moon Image.png")) std::cerr << "Failed to load Assets/DayNight/Moon Image.png\n";
+		tiles.push_back(std::move(t));
+	}
+
+	std::cout << "Textures Loaded: " << tiles.size() << std::endl;
 }
 
 sf::Texture& Assets::getTexture(int id) 
